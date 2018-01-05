a3bc2b4cd30fc395488855ac342db030e1826151
paulpod <paulpod@neuromantics.net>
2018-01-05 12:57:16 +0000

    Added and made working the edit hours page for a pharmacy

--- app/routes.js
+++ app/routes.js
@@ -10,7 +10,35 @@ router.get('/', function (req, res) {
 
 
 
+// Branching
 
+router.get('/pharma-details', function (req, res) {
+  // get the answer from the query string 
+  var compare = req.query.reviewPharmaDetails
+
+  if (compare === 'false') {
+    // redirect to the relevant page
+    res.redirect('/edit-pharma-details')
+  } else {
+    // if function is any other value (or is missing) render the page requested
+    res.render('review-pharma-hours')
+  }
+})
+
+
+
+router.get('/pharma-hours', function (req, res) {
+  // get the answer from the query string 
+  var compare = req.query.reviewPharmaHours
+
+  if (compare === 'false') {
+    // redirect to the relevant page
+    res.redirect('/edit-pharma-hours')
+  } else {
+    // if function is any other value (or is missing) render the page requested
+    res.render('review-pharma-holidays')
+  }
+})
 
 
 module.exports = router
