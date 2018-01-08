f5cdfddb5e1d5ad61cb5f8ace75c937e3f9b4c77
paulpod <paulpod@neuromantics.net>
2018-01-08 16:25:12 +0000

    Added and made working the edit hours + review holidays

--- app/routes.js
+++ app/routes.js
@@ -41,4 +41,24 @@ router.get('/pharma-hours', function (req, res) {
 })
 
 
+
+
+router.get('/pharma-hols', function (req, res) {
+  // get the answer from the query string 
+  var compare = req.query.reviewPharmaHols
+
+  if (compare === 'false') {
+    // redirect to the relevant page
+    res.redirect('/edit-pharma-holidays')
+  } else {
+    // if function is any other value (or is missing) render the page requested
+    //
+    // The next step after reviewing/editing the pharmacy details, hours, holidays
+    // could be the hours of the services, but could equally be a service search, select
+    // review, edit or other next step that best fulfils the user needs at this point
+    res.render('review-service-hours')
+  }
+})
+
+
 module.exports = router
