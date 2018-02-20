73ed0efe87dc11186b611a15d216cda03afc8a58
paulpod <paulpod@neuromantics.net>
2018-02-20 13:42:34 +0000

    added a first stab at a services opening hours page ... are they the same as the Pharmacy, or not

--- app/routes.js
+++ app/routes.js
@@ -47,32 +47,9 @@ router.get('/pharma-hols', function (req, res) {
   // get the answer from the query string 
   var compare = req.query.reviewPharmaHols
 
-  if (compare === 'false') {
-    // redirect to the relevant page
-    res.redirect('/edit-pharma-holidays')
-  } else {
-    // if function is any other value (or is missing) render the page requested
-    //
-    // The next step after reviewing/editing the pharmacy details, hours, holidays
-    // could be the hours of the services, but could equally be a service search, select
-    // review, edit or other next step that best fulfils the user needs at this point
-    res.render('review-service-hours')
-  }
+  res.render('review-pharma-servicehours')
 })
 
-
-router.get('/pharma-servicehours', function (req, res) {
-  // get the answer from the query string 
-  var compare = req.query.reviewPharmaServiceHours
-
-  if (compare === 'false') {
-    // redirect to the relevant page
-    res.redirect('/edit-pharma-service-hours')
-  } else {
-    // if function is any other value (or is missing) render the page requested
-    res.render('review-pharma-service-holidays')
-  }
-})
 
 
 module.exports = router
